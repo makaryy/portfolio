df7949da0a037714958a7c361cdff116503e7950
Makary Pagacz <pagaczmakary@gmail.com>
2023-12-21 18:04:33 +0100

    refactor: add tesla-system to constants, opitmize images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                hostname: "res.cloudinary.com",
+                pathname: `/${process.env.CLOUDINARY_CLOUD_NAME}/**`
+            }
+        ]
+    }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
